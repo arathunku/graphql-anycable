bacdd5edfab51f459c88fee73e28bf19b9b58363
Andrey Novikov <envek@envek.name>
2020-08-28 18:17:47 +0300

    Implement GraphQL-Ruby 1.11+ Broadcasting feature support.

--- lib/graphql/subscriptions/anycable_subscriptions.rb
+++ lib/graphql/subscriptions/anycable_subscriptions.rb
@@ -58,8 +58,11 @@ module GraphQL
 
       SUBSCRIPTION_PREFIX = "graphql-subscription:"
       SUBSCRIPTION_EVENTS_PREFIX = "graphql-subscription-events:"
-      EVENT_PREFIX = "graphql-event:"
+      FINGERPRINTS_PREFIX = "graphql-fingerprints:"  # To get fingerprints by topic
+      SUBSCRIPTIONS_PREFIX = "graphql-subcriptions:" # To get subscriptions by fingerprint
       CHANNEL_PREFIX = "graphql-channel:"
+      # For backward compatibility:
+      EVENT_PREFIX = "graphql-event:"
 
       # @param serializer [<#dump(obj), #load(string)] Used for serializing messages before handing them to `.broadcast(msg)`
       def initialize(serializer: Serialize, **rest)
@@ -70,6 +73,37 @@ module GraphQL
       # An event was triggered.
       # Re-evaluate all subscribed queries and push the data over ActionCable.
       def execute_all(event, object)
+        fingerprints = redis.smembers(FINGERPRINTS_PREFIX + event.topic)
+        return if fingerprints.empty?
+
+        grouped_subscription_ids =
+          redis.pipelined do
+            fingerprints.each do |fingerprint|
+              redis.smembers(SUBSCRIPTIONS_PREFIX + fingerprint)
+            end
+          end
+
+        grouped_subscription_ids.each do |subscription_ids|
+          execute_grouped(subscription_ids, event, object)
+        end
+
+        execute_legacy(event, object)
+      end
+
+      def execute_grouped(subscription_ids, event, object)
+        return if subscription_ids.empty?
+
+        # The fingerprint has told us that this response should be shared by all subscribers,
+        # so just run it once, then deliver the result to every subscriber
+        result = execute_update(subscription_ids.first, event, object)
+        # Having calculated the result _once_, send the same payload to all subscribers
+        subscription_ids.each do |subscription_id|
+          deliver(subscription_id, result)
+        end
+      end
+
+      # For migration from pre-1.0 graphql-anycable gem
+      def execute_legacy(event, object)
         redis.smembers(EVENT_PREFIX + event.topic).each do |subscription_id|
           next unless redis.exists?(SUBSCRIPTION_PREFIX + subscription_id)
           execute(subscription_id, event, object)
@@ -86,10 +120,17 @@ module GraphQL
       # Save query to "storage" (in redis)
       def write_subscription(query, events)
         context = query.context.to_h
-        subscription_id = context[:subscription_id] ||= build_id
+        subscription_id = context.delete(:subscription_id) || build_id
         channel = context.delete(:channel)
-        stream = context[:action_cable_stream] ||= SUBSCRIPTION_PREFIX + subscription_id
+        stream = context.delete(:action_cable_stream) || SUBSCRIPTION_PREFIX + subscription_id
         channel.stream_from(stream)
+
+        # Maybe we can eliminate intermediate step in (topic→fingerprint→subscription_id) and stream right by event fingerprint?
+        # This :action_cable_stream param most probably can be dropped. See https://github.com/rmosolgo/graphql-ruby/pull/3076
+        #
+        # events.group_by(&:fingerprint).each_key do |fingerprint|
+        #   channel.stream_from(SUBSCRIPTION_PREFIX + fingerprint)
+        # end
 
         data = {
           query_string: query.query_string,
@@ -103,7 +144,8 @@ module GraphQL
           redis.mapped_hmset(SUBSCRIPTION_PREFIX + subscription_id, data)
           redis.sadd(SUBSCRIPTION_EVENTS_PREFIX + subscription_id, events.map(&:topic))
           events.each do |event|
-            redis.sadd(EVENT_PREFIX + event.topic, subscription_id)
+            redis.sadd(FINGERPRINTS_PREFIX + event.topic, event.fingerprint)
+            redis.sadd(SUBSCRIPTIONS_PREFIX + event.fingerprint, subscription_id)
           end
           next unless config.subscription_expiration_seconds
           redis.expire(CHANNEL_PREFIX + channel.params["channelId"], config.subscription_expiration_seconds)
